7519c049e5102af9fb66d93a60d3ab16e51d9e84
Miguel Alcalde <miguel.alcalde@vercel.com>
2024-08-13 17:32:36 +0200

    Add vercel toolbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,6 @@ export default function Home() {
           </a>
         </div>
       </div>
-
       <div className="gap-10 relative flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <TooltipProvider>
           <Tooltip>
@@ -112,6 +111,7 @@ export default function Home() {
           </Tooltip>
         </TooltipProvider>
       </div>
+      <h2 className="font-extralight text-4xl -mt-40">Demo starter kit</h2>
 
       <div className="flex mb-32 gap-5 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a

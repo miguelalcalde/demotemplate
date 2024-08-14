9725a22b802e60db429aaa3e024634b57b1bdda1
Miguel Alcalde <miguel.alcalde@vercel.com>
2024-08-14 10:47:08 +0200

    add vercel toolbar next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Config options here
+};
+import vercelToolbar from "@vercel/toolbar/plugins/next";
 
-export default nextConfig;
+const withVercelToolbar = vercelToolbar();
+// Instead of module.exports = nextConfig, do this:
+export default withVercelToolbar(nextConfig);
